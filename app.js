dba378afd10e7d555188c2b2615973477ffe8f05
Devon Lambert <devon@realityknights.com>
2019-10-05 11:12:59 -0500

    Fixes SASS setup and moves SASS folder to root

--- app.js
+++ app.js
@@ -18,11 +18,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: false, // true = .sass and false = .scss
+  src: 'sass',
+  dest: path.join(__dirname, 'public/stylesheets'),
+  prefix: '/stylesheets',
+  debug: true,
   sourceMap: false
 }));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
